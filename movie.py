a9460d3a59e57b5a0850e03016a78faed8ae9d77
Santhini S <santhinisj@gmail.com>
2017-03-24 12:05:23 +0530

    Added comments

--- movie.py
+++ movie.py
@@ -1,9 +1,24 @@
+# This module defines a class 'Movie' and its data members and member functions
+
 import webbrowser
+
 class Movie():
-    def __init__(self, movie_name , movie_storyline, movie_poster, movie_trailer):
+    ''' This class is used for storing data related to a movie'''
+    
+    def __init__(
+                    self, movie_name, movie_storyline,
+                    movie_poster, movie_trailer
+                    ):
+        # Constructor
+        
         self.title = movie_name
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
-    def show_Trailer(self):
+        
+    def show_Trailer(
+                        self
+                        ):
+        #This function opens the movie trailer in browser
+        
         webbrowser.open(self.trailer)
